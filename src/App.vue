cbce779e1d0c97ff2451c1fcf72827ffe34363c4
jcrz <jfuentes.dev@gmail.com>
2020-07-28 00:56:27 -0400

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <Header titulo="Tareas por aprender" />
+    <div class="contenedor">
+      <Lista
+        titulo="Tech"
+        v-bind:tech="tech"
+      />
+      <Lista
+        titulo="Financial"
+        v-bind:tech="Financial"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Lista from "./components/Lista.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Lista
+  },
+  data: function(){
+    return {
+      tech: [
+        { texto: 'HTML', url: 'https://www.google.com' },
+        { texto: 'CSS', url: 'https://www.google.com' },
+        { texto: 'Vue', url: 'https://www.google.com' },
+        { texto: 'Firebase', url: 'https://www.google.com' }
+      ],
+      Financial: [
+        { texto: 'Investing', url: 'https://www.google.com' },
+        { texto: 'Budgeting', url: 'https://www.google.com' },
+        { texto: 'Balancing a checkbook', url: 'https://www.google.com' }
+      ]
+    }
+  }
+};
+</script>
+
+<style>
+.contenedor {
+  width: 500px;
+  margin: 50px auto;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Lista.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="lista">
+    <h4>{{ titulo }}</h4>
+
+    <ul class="list-group">
+        <li class="list-group-item" v-for="(item, i) in tech" v-bind:key="i">
+            <a v-bind:href="item.url">{{ item.texto }}</a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Lista",
+    props: {
+        titulo: String,
+        tech: []
+    }
+};
+</script>
+
+<style scoped>
+h4 {
+    color: red;
+    padding-left: 10px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin-bottom: 20px;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
